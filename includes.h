c144203c32d888fa594a548170d576c10408cfbd
Khor Zhen Win <khorzhenwin@gmail.com>
2022-05-14 17:29:08 +0800

    :alarm_clock: Initialize empty files for architecture

---
+++ includes.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// pragma once is to ensure includes dont get inserted multiple times into the same file
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+#include <time.h>
